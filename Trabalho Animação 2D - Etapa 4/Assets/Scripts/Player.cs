f3778b9d467047e3867b935761e43fde7712d3e7
renatasantox <renatarafaelapereiradejesis@gmail.com>
2026-02-05 15:40:49 -0300

    andando e idle

--- Trabalho Animação 2D - Etapa 4/Assets/Scripts/Player.cs
+++ Trabalho Animação 2D - Etapa 4/Assets/Scripts/Player.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float velocidade = 5f;
+    public float forcaPulo = 7f;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    private bool andando;
+    private bool noChao;
+
+    public Transform verificadorChao;
+    public LayerMask layerChao;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Movimento horizontal
+        float movimento = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(movimento * velocidade, rb.linearVelocity.y);
+
+        // Flip do sprite
+        if (movimento > 0)
+            spriteRenderer.flipX = false;
+        else if (movimento < 0)
+            spriteRenderer.flipX = true;
+
+        // Verifica se está no chão
+        noChao = Physics2D.OverlapCircle(verificadorChao.position, 0.2f, layerChao);
+
+        // Pulo
+        if (Input.GetKeyDown(KeyCode.Space) && noChao)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, forcaPulo);
+        }
+
+        // Animações
+        andando = movimento != 0;
+        animator.SetBool("Andando", andando);
+        animator.SetBool("NoChao", noChao);
     }
 }
